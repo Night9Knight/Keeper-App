0e22f20948bc87d90bf8f4a05388918b7deb5e11
Night9Knight <tang.ws998@gmail.com>
2020-10-21 22:41:31 +0800

    Updates

---
+++ precache-manifest.d07469f9bd06edb630ccc8e2eca9d3b1.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "b2555705dbcc0684e737d1f8f5b7645e",
+    "url": "/Night9Knight/Keeper-App/index.html"
+  },
+  {
+    "revision": "3706ab12ef6620f5d22d",
+    "url": "/Night9Knight/Keeper-App/static/js/2.1ffe84ea.chunk.js"
+  },
+  {
+    "revision": "1260a01290ac0c0454e9a7627323041c",
+    "url": "/Night9Knight/Keeper-App/static/js/2.1ffe84ea.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "a00d3682df5305ef398c",
+    "url": "/Night9Knight/Keeper-App/static/js/main.e0ccbe57.chunk.js"
+  },
+  {
+    "revision": "7abd9970375d652e5cc2",
+    "url": "/Night9Knight/Keeper-App/static/js/runtime-main.504fbccf.js"
+  }
+]);
