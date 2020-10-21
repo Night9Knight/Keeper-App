e66a18f6b83d06a60737f50901ad63cff6927d0b
Night9Knight <tang.ws998@gmail.com>
2020-10-21 23:00:05 +0800

    Updates

---
+++ precache-manifest.65fa49d09eff650b2a2bc0549e131d24.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "37c6eb21ed1c923a2f08bfacb3305cca",
+    "url": "/Keeper-App/index.html"
+  },
+  {
+    "revision": "3706ab12ef6620f5d22d",
+    "url": "/Keeper-App/static/js/2.1ffe84ea.chunk.js"
+  },
+  {
+    "revision": "1260a01290ac0c0454e9a7627323041c",
+    "url": "/Keeper-App/static/js/2.1ffe84ea.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "a00d3682df5305ef398c",
+    "url": "/Keeper-App/static/js/main.e0ccbe57.chunk.js"
+  },
+  {
+    "revision": "f413b374dd586d74b375",
+    "url": "/Keeper-App/static/js/runtime-main.d0d0a995.js"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/Night9Knight/Keeper-App/precache-manifest.d07469f9bd06edb630ccc8e2eca9d3b1.js"
+  "/Keeper-App/precache-manifest.65fa49d09eff650b2a2bc0549e131d24.js"
 );
 
 self.addEventListener('message', (event) => {
@@ -33,7 +33,7 @@ workbox.core.clientsClaim();
 self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/Night9Knight/Keeper-App/index.html"), {
+workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/Keeper-App/index.html"), {
   
   blacklist: [/^\/_/,/\/[^/?]+\.[^/]+$/],
 });
